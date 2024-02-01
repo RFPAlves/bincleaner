71a4a73974c5dbfe95f9a12fa3350e402b0a4f83
Rafael de Paula <rfpalves@yahoo.com.br>
2024-02-01 16:31:09 -0300

    A bin and obj folder cleaner.

---
+++ binclear.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+usage() {
+  echo "USAGE: $0 --path <path_to_dir> [--confirm]"
+}
+
+if [ $# -lt 2 ]; then
+  usage
+  exit 1
+fi
+
+CONFIRM='no'
+
+while [ $# -gt 0 ]; do
+  COMMAND=$1
+  case $COMMAND in
+    --path)
+      DIRECTORY=$2
+      shift 2
+      ;;
+    --confirm)
+      CONFIRM='yes'
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [[ ! -d $DIRECTORY ]]; then
+  echo "Directory ${DIRECTORY} does not exists."
+  usage
+  exit 1
+fi
+
+set -euo pipefail
+
+pushd $DIRECTORY >/dev/null 2>&1
+
+exec 5>&1
+folders=()
+
+IFS=$'\n'
+for dir in $(find . -type d | grep 'obj$\|bin$'); do
+  FILENAME_SIZE=$(du "$dir" -s -h | tee /dev/fd/5)
+  FILENAME=$(echo $FILENAME_SIZE | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')
+
+  folders+=($FILENAME)
+done
+
+echo ""
+
+for value in ${folders[@]}; do
+  if [[ "$CONFIRM" == 'yes' ]]; then
+    printf "[${value}] Would you like to remove [Y/n]? "
+    read n
+
+    if [[ $n == 'q' ]]; then
+      break
+    fi
+
+    if [[ ! $n =~ [Yy] ]]; then
+      continue
+    fi
+  fi
+
+  rm -rf $value
+  printf "[${value}] Removed.\n"
+done
+
+popd >/dev/null 2>&1
